c74cc2c061a8bb3668d13639c32629818b800e89
James A. Brannan <jamesabrannan@hushmail.com>
2023-09-14 13:51:10 -0400

    changes to instructions

--- go.mod
+++ go.mod
@@ -2,10 +2,10 @@ module github.com/dynamicpdf-api/go-client-examples
 
 go 1.20
 
-require github.com/dynamicpdf-api/go-client v1.1.0
+require github.com/dynamicpdf-api/go-client v1.1.1
 
 require (
 	github.com/gabriel-vasile/mimetype v1.4.2 // indirect
-	github.com/google/uuid v1.3.0 // indirect
-	golang.org/x/net v0.12.0 // indirect
+	github.com/google/uuid v1.3.1 // indirect
+	golang.org/x/net v0.15.0 // indirect
 )
--- pdf-instructions-example.go
+++ pdf-instructions-example.go
@@ -12,66 +12,8 @@ import (
 	"github.com/dynamicpdf-api/go-client/resource"
 )
 
-func dlexResourceExample(key string, baseUrl string) *endpoint.Pdf {
-
-	pdfD := endpoint.NewPdf()
-	pdfD.AddDlexWithCloudResourceNLayoutDataPath("samples/getting-started/getting-started.dlex", "C:/temp/dynamicpdf-api-samples/getting-started.json")
-	pdfD.Endpoint.BaseUrl = baseUrl
-	pdfD.Endpoint.ApiKey = key
-
-	return pdfD
-}
-
-func pdfFromResourceExample(key string, baseUrl string) *endpoint.Pdf {
-	pr := endpoint.NewPdf()
-	mergeOption := input.NewMergeOptions()
-	prInput := input.NewPdfWithCloudPath("example/DocumentB.pdf", mergeOption)
-	pr.Inputs = append(pr.Inputs, prInput)
-	pr.Endpoint.BaseUrl = baseUrl
-	pr.Endpoint.ApiKey = key
-
-	pdfResource := resource.NewPdfResourceWithResourcePath("c:/temp/dynamicpdf-api-samples/DocumentA.pdf", "DocumentA.pdf")
-	prInput2 := input.NewPdfWithResource(pdfResource)
-	pr.Inputs = append(pr.Inputs, prInput2)
-
-	return pr
-}
-
-func securityExample(key string, baseUrl string) *endpoint.Pdf {
-	secPdf := endpoint.NewPdf()
-	secRes := resource.NewPdfResourceWithResourcePath("c:/temp/dynamicpdf-api-samples/DocumentA.pdf", "DocumentA.pdf")
-	secInput := input.NewPdfWithResource(secRes)
-	secPdf.Endpoint.BaseUrl = baseUrl
-	secPdf.Endpoint.ApiKey = key
-
-	security := endpoint.NewAes128Security("user", "owner")
-	secPdf.Security = endpoint.Security(security.Security)
-	secPdf.Inputs = append(secPdf.Inputs, secInput)
-	return secPdf
-}
-
-func barcodeExample(key string, baseUrl string) *endpoint.Pdf {
-	barcodePdf := endpoint.NewPdf()
-	barcodePdf.Endpoint.BaseUrl = baseUrl
-	barcodePdf.Endpoint.ApiKey = key
-
-	barcode := element.NewAztecBarcodeElement("Hello World", element.BottomRight, 50.0, 50.0)
-	barcode.SetSymbolSize(element.R105xC105)
-	barcode.SetXDimension(2)
-	barcode.XOffset = 0
-	barcode.YOffset = 10
-	pdfInput := input.NewPage()
-	pdfInput.Elements = append(pdfInput.Elements, barcode)
-	barcodePdf.Inputs = append(barcodePdf.Inputs, pdfInput)
-
-	return barcodePdf
-}
-
-func topLevelMetadata(key string, baseUrl string) *endpoint.Pdf {
+func topLevelMetadata() *endpoint.Pdf {
 	tlm := endpoint.NewPdf()
-	tlm.Endpoint.BaseUrl = baseUrl
-	tlm.Endpoint.ApiKey = key
-
 	pageInput := input.NewPage()
 	tlm.Author = "John Doe"
 	tlm.Creator = "John Creator"
@@ -85,71 +27,23 @@ func topLevelMetadata(key string, baseUrl string) *endpoint.Pdf {
 	return tlm
 }
 
-func  googleFontsExample(key string, b baseUrl string) *endpoint.PDF {
-
-	pdfCl := endpoint.NewPdf()
-	pdfCl.Endpoint.BaseUrl = baseUrl
-	pdfCl.Endpoint.ApiKey = key
-
-	pageInput := input.NewPage()
-	pageNumber := element.NewPageNumberingElement("test", "TopLeft", 0, 0)
-	pageNumber.SetFontSize(72)
-	pageNumber.TextFont()
-	pageNumber.SetFont(*font.Google("Borel"))
-
-	pageColor := color.NewRgbColorDefault().Red()
-	pageNumber.SetColor(pageColor.Color)
-
-	myTextElement := element.NewText("myTest", element.TopLeft, 100, 200)
-	myTextElement.SetFont(*font.Google("Borel"))
-
-	pageInput.Elements = append(pageInput.Elements, myTextElement)
-
-	pageInput.Elements = append(pageInput.Elements, pageNumber)
-	pdfCl.Inputs = append(pdfCl.Inputs, pageInput)
-
-	return pdfC1
+func mergeExample(basePath string) *endpoint.Pdf {
+	pr := endpoint.NewPdf()
+	mergeOption := input.NewMergeOptions()
+	prInput := input.NewPdfWithCloudPath("samples/users-guide-resources/DocumentB.pdf", mergeOption)
+	pr.Inputs = append(pr.Inputs, prInput)
+	imageResource := resource.NewImageResourceWithResourcePath(basePath+"DPDFLogo.png", "DPDFLogo.png")
+	pr.AddImage(imageResource)
+	pdfResource := resource.NewPdfResourceWithResourcePath(basePath+"DocumentA.pdf", "DocumentA.pdf")
+	prInput2 := input.NewPdfWithResource(pdfResource)
+	pr.Inputs = append(pr.Inputs, prInput2)
+	return pr
 }
 
-func fontsExample(key string, baseUrl string) *endpoint.Pdf {
+func acroFormExample(basePath string) *endpoint.Pdf {
 
-	fe := endpoint.NewPdf()
-	fe.Endpoint.BaseUrl = baseUrl
-	fe.Endpoint.ApiKey = key
-
-	pageInput := input.NewPage()
-	pageNumber := element.NewPageNumberingElement("A", "TopLeft", 0, 0)
-	pageNumber.SetFontSize(42)
-	pageNumber.SetFont(font.HelveticaBoldOblique())
-
-	pageColor := color.NewRgbColorDefault().Red()
-	pageNumber.SetColor(pageColor.Color)
-
-	pageNumberingElementTwo := element.NewPageNumberingElement("B", "TopRight", 0, 0)
-	pageNumberingElementTwo.SetColor(color.NewRgbColorDefault().DarkGreen().Color)
-	myFont := font.NewFontResource("c:/temp/fonts-example/cnr.otf", "cnr.otf")
-	pageNumberingElementTwo.SetFont(*myFont)
-	pageNumberingElementTwo.SetFontSize(32)
-
-	pageInput.Elements = append(pageInput.Elements, pageNumber)
-	pageInput.Elements = append(pageInput.Elements, pageNumberingElementTwo)
-	fe.Inputs = append(fe.Inputs, pageInput)
-	return fe
-}
-
-func imageExample(key string, baseUrl string) *endpoint.Pdf {
-	prImage := endpoint.NewPdf()
-	imageResource := resource.NewImageResourceWithResourcePath("C:/temp/dynamicpdf-api-samples/image-info/getting-started.png", "getting-started.png")
-	prImage.AddImage(imageResource)
-	prImage.AddImageCloudPath("samples/image-info/getting-started.png")
-	prImage.Endpoint.ApiKey = key
-	prImage.Endpoint.BaseUrl = baseUrl
-	return prImage
-}
-
-func acroFormExample(key string, baseUrl string) *endpoint.Pdf {
 	pdfAcro := endpoint.NewPdf()
-	pdfResource := resource.NewPdfResourceWithResourcePath("C:/temp/dynamicpdf-api-samples/simple-form-fill.pdf", "simple-form.pdf")
+	pdfResource := resource.NewPdfResourceWithResourcePath(basePath+"simple-form-fill.pdf", "simple-form.pdf")
 	pdfInput := input.NewPdfWithResource(pdfResource)
 	pdfAcro.Inputs = append(pdfAcro.Inputs, pdfInput)
 
@@ -158,14 +52,10 @@ func acroFormExample(key string, baseUrl string) *endpoint.Pdf {
 	field2 := endpoint.NewFormFieldWithValue("descriptionField", "RealRTime Pdf's. Real FAST!")
 	pdfAcro.FormFields = append(pdfAcro.FormFields, *field2)
 
-	pdfAcro.Endpoint.ApiKey = key
-	pdfAcro.Endpoint.BaseUrl = baseUrl
-
 	return pdfAcro
 }
 
-/*
-func outlineExample(key string, baseUrl string) *endpoint.Pdf {
+func outlineExample() *endpoint.Pdf {
 
 	pdfOut := endpoint.NewPdf()
 
@@ -185,75 +75,182 @@ func outlineExample(key string, baseUrl string) *endpoint.Pdf {
 
 	outline := pdfOut.Outlines.Add("Root Outline")
 	outline.Expanded = true
-	out := outline.Children()
-	out.AddWithInputValue("Page 1", pageInputOne, 1, endpoint.FitPage)
-	out.AddWithInputValue("Page 2", pageInputTwo, 2, endpoint.FitPage)
-	out.AddWithInputValue("Page 3", pageInputOne, 3, endpoint.FitPage)
 
-	pdfOut.Endpoint.ApiKey = key
-	pdfOut.Endpoint.BaseUrl = baseUrl
+	out := outline.Children()
+
+	out.AddWithInputValue("Page 1", pageInputOne.Input, 1, endpoint.FitPage)
+	out.AddWithInputValue("Page 2", pageInputTwo.Input, 2, endpoint.FitPage)
+	out.AddWithInputValue("Page 3", pageInputOne.Input, 3, endpoint.FitPage)
 
 	return pdfOut
 }
-*/
 
-func templateExample(key string, baseUrl string) *endpoint.Pdf {
+func imageExample(basePath string) *endpoint.Pdf {
+	prImage := endpoint.NewPdf()
+	imageResource := resource.NewImageResourceWithResourcePath(basePath+"DPDFLogo.png", "DPDFLogo.png")
+	prImage.AddImage(imageResource)
+	prImage.AddImageCloudPath("samples/users-guide-resources/A.png")
+	return prImage
+}
+
+func barcodeExample() *endpoint.Pdf {
+	barcodePdf := endpoint.NewPdf()
+	barcode := element.NewAztecBarcodeElement("Hello World", element.BottomRight, 50.0, 50.0)
+	barcode.SetSymbolSize(element.R105xC105)
+	barcode.SetXDimension(2)
+	barcode.XOffset = 0
+	barcode.YOffset = 10
+	pdfInput := input.NewPage()
+	pdfInput.Elements = append(pdfInput.Elements, barcode)
+	barcodePdf.Inputs = append(barcodePdf.Inputs, pdfInput)
+
+	return barcodePdf
+}
+
+func fontsExample(basePath string) *endpoint.Pdf {
+
+	fe := endpoint.NewPdf()
+
+	pageInput := input.NewPage()
+	pageNumber := element.NewPageNumberingElement("A", "TopLeft", 0, 0)
+	pageNumber.SetFontSize(42)
+	pageNumber.SetFont(font.HelveticaBoldOblique())
+
+	pageColor := color.NewRgbColorDefault().Red()
+	pageNumber.SetColor(pageColor.Color)
+
+	pageNumberingElementTwo := element.NewPageNumberingElement("B", "TopRight", 0, 0)
+	pageNumberingElementTwo.SetColor(color.NewRgbColorDefault().DarkGreen().Color)
+	myFont := font.NewFontResource(basePath+"cnr.otf", "cnr.otf")
+	pageNumberingElementTwo.SetFont(*myFont)
+	pageNumberingElementTwo.SetFontSize(32)
+
+	pageInput.Elements = append(pageInput.Elements, pageNumber)
+	pageInput.Elements = append(pageInput.Elements, pageNumberingElementTwo)
+	fe.Inputs = append(fe.Inputs, pageInput)
+	return fe
+}
+
+func securityExample(basePath string) *endpoint.Pdf {
+	secPdf := endpoint.NewPdf()
+	secRes := resource.NewPdfResourceWithResourcePath(basePath+"DocumentA.pdf", "DocumentA.pdf")
+	secInput := input.NewPdfWithResource(secRes)
+
+	security := endpoint.NewAes128Security("user", "owner")
+	secPdf.Security = endpoint.Security(security.Security)
+	secPdf.Inputs = append(secPdf.Inputs, secInput)
+	return secPdf
+}
+
+func pdfHtmlExample() *endpoint.Pdf {
+
+	pdfExample := endpoint.NewPdf()
+
+	inputOne := input.NewHtmlInputWithString("<html>An example HTML fragment.</html>")
+
+	inputTwo := input.NewHtmlInputWithString("<html><p>HTML with basePath.</p><img src='./images/logo.png'></html>")
+	inputTwo.SetBasePath("https://www.dynamicpdf.com")
+
+	pdfExample.Inputs = append(pdfExample.Inputs, inputOne)
+	pdfExample.Inputs = append(pdfExample.Inputs, inputTwo)
+
+	return pdfExample
+}
+
+func templateExample(basePath string) *endpoint.Pdf {
 	pdfTemp := endpoint.NewPdf()
-
-	pdfTemp.Endpoint.BaseUrl = baseUrl
-	pdfTemp.Endpoint.ApiKey = key
-
-	pdfResource := resource.NewPdfResourceWithResourcePath("c:/temp/dynamicpdf-api-samples/DocumentA.pdf", "DocumentA.pdf")
+	pdfResource := resource.NewPdfResourceWithResourcePath(basePath+"DocumentA.pdf", "DocumentA.pdf")
 	pdfInput1 := input.NewPdfWithResource(pdfResource)
-
 	temp := element.NewTemplate()
 	txt := element.NewText("Hello World", element.TopCenter, 20, 10)
 	txt.SetColor(color.NewRgbColorDefault().Red().Color)
 	txt.SetFontSize(32)
-
 	temp.Elements = append(temp.Elements, txt)
 	pdfInput1.SetTemplate(temp)
-
 	pdfTemp.Inputs = append(pdfTemp.Inputs, pdfInput1)
-
 	return pdfTemp
+}
 
+func dlexResourceExample(basePath string) *endpoint.Pdf {
+	pdfD := endpoint.NewPdf()
+	pdfD.AddDlexWithCloudResourceNLayoutDataPath("samples/users-guide-resources/SimpleReportWithCoverPage.dlex",
+		basePath+"SimpleReportWithCoverPage.json")
+	return pdfD
+}
+
+func googleFontsExample() *endpoint.Pdf {
+
+	pdfCl := endpoint.NewPdf()
+
+	pageInput := input.NewPage()
+	pageNumber := element.NewPageNumberingElement("test", "TopLeft", 0, 0)
+	pageNumber.SetFontSize(72)
+	pageNumber.TextFont()
+	pageNumber.SetFont(*font.Google("Borel"))
+
+	pageColor := color.NewRgbColorDefault().Red()
+	pageNumber.SetColor(pageColor.Color)
+
+	myTextElement := element.NewText("myTest", element.TopLeft, 100, 200)
+	myTextElement.SetFont(*font.Google("Borel"))
+
+	pageInput.Elements = append(pageInput.Elements, myTextElement)
+
+	pageInput.Elements = append(pageInput.Elements, pageNumber)
+	pdfCl.Inputs = append(pdfCl.Inputs, pageInput)
+
+	return pdfCl
 }
 
 func main() {
 
-	//pdfTemp := templateExample("DP---API-KEY---", "https://api.dynamicpdf.com/")
-	//process(pdfTemp, "c:/temp/dynamicpdf-api-samples/pdfTempExample.pdf")
+	var theBasePath = "c:/temp/users-guide-resources/"
+	var theBaseUrl = "https://api.dynamicpdf.com/"
+	var theOutputPath = "c:/temp/dynamicpdf-api-usersguide-examples/go-output/"
+	var apiKey = "DP.f9wgIYZzDrTajG2MSAWUdh1m7YJ1+iexxbkV/15t0l3TR5oaxRjdGCC1"
 
-	//	pdfOut := outlineExample("DP---API-KEY---", "https://api.dynamicpdf.com/")
-	//	process(pdfOut,"c:/temp/dynamicpdf-api-samples/pdfOutlineExample.pdf")
+	pdfTlm := topLevelMetadata()
+	process(pdfTlm, theOutputPath+"toplevelmetadata-ouput.pdf", theBaseUrl, apiKey)
 
-	//	pdfAcro := acroFormExample("DP---API-KEY---", "https://api.dynamicpdf.com/")
-	//	process(pdfAcro,"c:/temp/dynamicpdf-api-samples/pdfAcroExample.pdf")
+	pdfAcro := acroFormExample(theBasePath)
+	process(pdfAcro, theOutputPath+"pdfAcroExample-output.pdf", theBaseUrl, apiKey)
 
-	//pdfD := dlexResourceExample("DP---API-KEY---", "https://api.dynamicpdf.com/")
-	//process(pdfD,"c:/temp/dynamicpdf-api-samples/pdfdlexExample.pdf")
+	pdfBar := barcodeExample()
+	process(pdfBar, theOutputPath+"bar-output.pdf", theBaseUrl, apiKey)
 
-	pdfIm := imageExample("DP---API-KEY---", "https://api.dynamicpdf.com/")
-	process(pdfIm, "c:/temp/dynamicpdf-api-samples/out/pdfImage.pdf")
+	pdfFnt := fontsExample(theBasePath)
+	process(pdfFnt, theOutputPath+"fnt-output.pdf", theBaseUrl, apiKey)
 
-	///pdfTlm := topLevelMetadata("DP---API-KEY---", "https://api.dynamicpdf.com/")
-	//process(pdfTlm, "c:/temp/dynamicpdf-api-samples/tlm.pdf")
+	pdfSec := securityExample(theBasePath)
+	process(pdfSec, theOutputPath+"sec-output.pdf", theBaseUrl, apiKey)
 
-	//pdfFnt := fontsExample("DP---API-KEY---", "https://api.dynamicpdf.com/")
-	//process(pdfFnt, "c:/temp/dynamicpdf-api-samples/fnt.pdf")
+	pdfHtmlExample := pdfHtmlExample()
+	process(pdfHtmlExample, theOutputPath+"pdfHtmlExample-output.pdf", theBaseUrl, apiKey)
 
-	//pdfBar := barcodeExample("DP---API-KEY---", "https://api.dynamicpdf.com/")
-	//process(pdfBar, "c:/temp/dynamicpdf-api-samples/bar.pdf")
+	pdfTemp := templateExample(theBasePath)
+	process(pdfTemp, theOutputPath+"pdfTempExample-output.pdf", theBaseUrl, apiKey)
 
-	//	pdfSec := securityExample("DP---API-KEY---", "https://api.dynamicpdf.com/")
-	//process(pdfSec, "c:/temp/dynamicpdf-api-samples/sec.pdf")
+	pdfGF := googleFontsExample()
+	process(pdfGF, theOutputPath+"pdfgooglefont-output.pdf", theBaseUrl, apiKey)
 
-	//pdfPr := pdfFromResourceExample("DP---API-KEY---", "https://api.dynamicpdf.com/")
-	//process(pdfPr, "c:/temp/dynamicpdf-api-samples/pdfPr.pdf")
+	//pdfdlexResourceExample := dlexResourceExample(theBasePath)
+	//process(pdfdlexResourceExample, theOutputPath +"dlexExample-output.pdf", theBaseUrl, apiKey)
+
+	//pdfIm := imageExample(theBasePath)
+	//process(pdfIm, theOutputPath + "pdfImage-output.pdf", theBaseUrl, apiKey)
+
+	//pdfOut := mergeExample(theBasePath)
+	//process(pdfOut, theOutputPath+"go-merge-example-output.pdf", theBaseUrl, apiKey)
+
+	//pdfOut := outlineExample()
+	//process(pdfOut, theOutputPath +"outline-example-output.pdf", theBaseUrl, apiKey)
+
 }
 
-func process(thePdf *endpoint.Pdf, outputFilePath string) {
+func process(thePdf *endpoint.Pdf, outputFilePath string, baseUrl string, apiKey string) {
+
+	thePdf.Endpoint.BaseUrl = baseUrl
+	thePdf.Endpoint.ApiKey = apiKey
 
 	resp := thePdf.Process()
 	res := <-resp
@@ -262,6 +259,7 @@ func process(thePdf *endpoint.Pdf, outputFilePath string) {
 		os.WriteFile(outputFilePath,
 			res.Content().Bytes(), os.ModeType)
 	} else {
+		fmt.Println("Error Message:" + res.ErrorMessage())
 		fmt.Print(res.ErrorJson())
 	}
 
