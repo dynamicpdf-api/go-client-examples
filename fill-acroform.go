117eb4eb501e2d9a15901c1fffaa63ddb5450027
James A. Brannan <jamesabrannan@hushmail.com>
2023-10-23 15:21:21 -0400

    Create fill-acroform.go

---
+++ fill-acroform.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/dynamicpdf-api/go-client/endpoint"
+	"github.com/dynamicpdf-api/go-client/input"
+	"github.com/dynamicpdf-api/go-client/resource"
+)
+
+func main() {
+
+	pdfAcro := endpoint.NewPdf()
+	pdfAcro.Endpoint.BaseUrl = "https://api.dynamicpdf.com/"
+	pdfAcro.Endpoint.ApiKey = "DP.--API-KEY---"
+	basePath := "c:/temp/dynamicpdf-api-samples/"
+
+	pdfResource := resource.NewPdfResourceWithResourcePath(basePath+"fw9AcroForm_18.pdf", "fw9AcroForm_18.pdf")
+	pdfInput := input.NewPdfWithResource(pdfResource)
+	pdfAcro.Inputs = append(pdfAcro.Inputs, pdfInput)
+
+	field1 := endpoint.NewFormFieldWithValue("topmostSubform[0].Page1[0].f1_1[0]", "Any Company, Inc.")
+	pdfAcro.FormFields = append(pdfAcro.FormFields, *field1)
+	field2 := endpoint.NewFormFieldWithValue("topmostSubform[0].Page1[0].f1_2[0]", "Any Company")
+	pdfAcro.FormFields = append(pdfAcro.FormFields, *field2)
+	field3 := endpoint.NewFormFieldWithValue("topmostSubform[0].Page1[0].FederalClassification[0].c1_1[0]", "1")
+	pdfAcro.FormFields = append(pdfAcro.FormFields, *field3)
+	field4 := endpoint.NewFormFieldWithValue("topmostSubform[0].Page1[0].Address[0].f1_7[0]", "123 Main Street")
+	pdfAcro.FormFields = append(pdfAcro.FormFields, *field4)
+	field5 := endpoint.NewFormFieldWithValue("topmostSubform[0].Page1[0].Address[0].f1_8[0]", "Washington, DC  22222")
+	pdfAcro.FormFields = append(pdfAcro.FormFields, *field5)
+	field6 := endpoint.NewFormFieldWithValue("topmostSubform[0].Page1[0].f1_9[0]", "Any Requester")
+	pdfAcro.FormFields = append(pdfAcro.FormFields, *field6)
+	field7 := endpoint.NewFormFieldWithValue("topmostSubform[0].Page1[0].f1_10[0]", "17288825617")
+	pdfAcro.FormFields = append(pdfAcro.FormFields, *field7)
+	field8 := endpoint.NewFormFieldWithValue("topmostSubform[0].Page1[0].EmployerID[0].f1_14[0]", "52")
+	pdfAcro.FormFields = append(pdfAcro.FormFields, *field8)
+	field9 := endpoint.NewFormFieldWithValue("topmostSubform[0].Page1[0].EmployerID[0].f1_15[0]", "1234567")
+	pdfAcro.FormFields = append(pdfAcro.FormFields, *field9)
+
+	resp := pdfAcro.Process()
+	res := <-resp
+
+	if res.IsSuccessful() == false {
+		if res.ClientError() != nil {
+			fmt.Print("Failed with error: " + res.ClientError().Error())
+		} else {
+			fmt.Print("Failed with error: " + res.ErrorJson())
+		}
+	} else {
+		os.WriteFile(basePath+"acroform-pdfs-output.pdf",
+			res.Content().Bytes(), os.ModeType)
+	}
+}
