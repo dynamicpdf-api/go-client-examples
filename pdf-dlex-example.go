c36d991185affff9d18a59c70ad894bee7fcf9e1
James A. Brannan <jamesabrannan@hushmail.com>
2023-10-23 17:10:51 -0400

    Create pdf-dlex-example.go

---
+++ pdf-dlex-example.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"github.com/dynamicpdf-api/go-client/endpoint"
+	"github.com/dynamicpdf-api/go-client/resource"
+)
+
+func main() {
+
+	pr := endpoint.NewPdf()
+	pr.Endpoint.BaseUrl = "https://api.dynamicpdf.com/"
+	pr.Endpoint.ApiKey = "DP---API-KEY---"
+	basePath := "c:/temp/dynamicpdf-api-samples/"
+
+	layoutDataResource := resource.NewLayoutDataResource(basePath+"SimpleReportWithCoverPage.json", "SimpleReportWithCoverPage.json")
+
+	pr.AddDlexWithCloudResourceNLayoutData("samples/creating-pdf-pdf-endpoint/SimpleReportWithCoverPage.dlex", layoutDataResource)
+
+	resp := pr.Process()
+	res := <-resp
+
+	if res.IsSuccessful() == false {
+		if res.ClientError() != nil {
+			fmt.Print("Failed with error: " + res.ClientError().Error())
+		} else {
+			fmt.Print("Failed with error: " + res.ErrorJson())
+		}
+	} else {
+		os.WriteFile(basePath+"pdf-dlex-pdf-output.pdf",
+			res.Content().Bytes(), os.ModeType)
+	}
+
+}
