ff08c9c3f23f3e1603fb4d0810c7e6605ad3aa66
James A. Brannan <jamesabrannan@hushmail.com>
2024-01-02 16:59:21 -0500

    update dlex-layout started changes to resources folder

--- dlex-layout-example.go
+++ dlex-layout-example.go
@@ -10,16 +10,50 @@ import (
 
 func main() {
 
-	layoutDataResource := resource.NewLayoutDataResource("c:/temp/dynamicpdf-api-samples/creating-pdf-dlex-layout.json", "creating-pdf-dlex-layout.json")
+	processCloud()
+	processLocal()
 
-	layoutData := endpoint.NewDlexEndpoint("samples/creating-pdf-dlex-layout-endpoint/creating-pdf-dlex-layout.dlex", layoutDataResource)
+}
+
+func processCloud() {
+	layoutDataResource := resource.NewLayoutDataResource("./resources/dlex-layout/SimpleReportWithCoverPage.json", "SimpleReportWithCoverPage.json")
+
+	layoutData := endpoint.NewDlexEndpoint("samples/dlex-layout/SimpleReportWithCoverPage.dlex", layoutDataResource)
 	layoutData.Endpoint.BaseUrl = "https://api.dynamicpdf.com/"
-	layoutData.Endpoint.ApiKey = "DP.xxx-api-key-xxx"
+	layoutData.Endpoint.ApiKey = "DP--api-key--"
 	resp := layoutData.Process()
+
 	res := <-resp
 
 	if res.IsSuccessful() == true {
-		os.WriteFile("c:/temp/dynamicpdf-api-samples/dlex-layout-output.pdf", res.Content().Bytes(), os.ModeType)
+		os.WriteFile("./output/dlex-layout-output.pdf", res.Content().Bytes(), os.ModeType)
+	} else {
+		fmt.Println("errorId: " + res.ErrorId().String())
+		fmt.Println("errorMsg: " + res.ErrorMessage())
+		fmt.Println("Failed with error json: " + res.ErrorJson())
+	}
+}
+
+func processLocal() {
+	layoutDataResource := resource.NewLayoutDataResource("./resources/dlex-layout/SimpleReportWithCoverPage.json", "SimpleReportWithCoverPage.json")
+
+	theDlexResource := resource.NewDlexResourceWithPath("./resources/dlex-layout/SimpleReportWithCoverPage.dlex", "SimpleReportWithCoverPage.dlex")
+
+
+	layoutData := endpoint.NewDlexEndpointWithResource(*theDlexResource, layoutDataResource)
+
+	additionalResource := endpoint.NewDlexWithAdditionalResource("./resources/dlex-layout/NorthwindLogo.gif", "NorthwindLogo.gif")
+
+	layoutData.Resources = append(layoutData.Resources, additionalResource);
+
+	layoutData.Endpoint.BaseUrl = "https://api.dynamicpdf.com/"
+	layoutData.Endpoint.ApiKey = "DP--api-key--"
+	resp := layoutData.Process()
+
+	res := <-resp
+
+	if res.IsSuccessful() == true {
+		os.WriteFile("./output/dlex-layout-local-output.pdf", res.Content().Bytes(), os.ModeType)
 	} else {
 		fmt.Println("errorId: " + res.ErrorId().String())
 		fmt.Println("errorMsg: " + res.ErrorMessage())
--- go.mod
+++ go.mod
@@ -2,10 +2,10 @@ module github.com/dynamicpdf-api/go-client-examples
 
 go 1.20
 
-require github.com/dynamicpdf-api/go-client v1.1.1
+require github.com/dynamicpdf-api/go-client v1.3.0
 
 require (
-	github.com/gabriel-vasile/mimetype v1.4.2 // indirect
-	github.com/google/uuid v1.3.1 // indirect
-	golang.org/x/net v0.15.0 // indirect
+	github.com/gabriel-vasile/mimetype v1.4.3 // indirect
+	github.com/google/uuid v1.5.0 // indirect
+	golang.org/x/net v0.19.0 // indirect
 )
