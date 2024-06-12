0e7b04becf5f02493d511788dc3e6ccecb50ce7e
James A. Brannan <jamesabrannan@hushmail.com>
2024-06-12 14:48:18 -0400

    Update pdf-html-example.go

--- pdf-html-example.go
+++ pdf-html-example.go
@@ -6,26 +6,40 @@ import (
 
 	"github.com/dynamicpdf-api/go-client/endpoint"
 	"github.com/dynamicpdf-api/go-client/input"
+	"github.com/dynamicpdf-api/go-client/resource"
 )
 
 func main() {
 
+	baseUrl := "https://api.dynamicpdf.com/"
+	apiKey := "DP--api-key--"
+	basePath := "./resources/converting-html-pdf-endpoint/"
+	outputPath := "./output/"
+
 	pdfExample := endpoint.NewPdf()
-	pdfExample.Endpoint.BaseUrl = "https://api.dynamicpdf.com/"
-	pdfExample.Endpoint.ApiKey = "DP.xxx-api-key-xxx"
+	pdfExample.Endpoint.BaseUrl = baseUrl
+	pdfExample.Endpoint.ApiKey = apiKey
 
 	inputOne := input.NewHtmlInputWithString("<html>An example HTML fragment.</html>")
 
 	inputTwo := input.NewHtmlInputWithString("<html><p>HTML with basePath.</p><img src='./images/logo.png'></html>")
 	inputTwo.SetBasePath("https://www.dynamicpdf.com")
 
+	htmlResource := resource.NewHtmlResource(basePath+"products.html", "products.html")
+
+	inputThree := input.NewHtmlInputWithResource(htmlResource)
+
 	pdfExample.Inputs = append(pdfExample.Inputs, inputOne)
 	pdfExample.Inputs = append(pdfExample.Inputs, inputTwo)
-    resp := pdfExample.Process()
+	pdfExample.Inputs = append(pdfExample.Inputs, inputThree)
+
+	resp := pdfExample.Process()
+	res := <-resp
+
 	if res.IsSuccessful() == true {
-		os.WriteFile("C:/temp/dynamicpdf-api-samples/pdf-html-example-output.pdf", 
-		res.Content().Bytes(), os.ModeType)
-	}else {
+		os.WriteFile(outputPath+"pdf-html-go-output.pdf",
+			res.Content().Bytes(), os.ModeType)
+	} else {
 		fmt.Print(res.ErrorJson())
 	}
 
