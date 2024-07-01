ac279b91fecdc2900687a90c3a7b59bc93714996
James A. Brannan <jamesabrannan@hushmail.com>
2024-07-01 11:25:15 -0400

    Update pdf-info-example.go

--- pdf-info-example.go
+++ pdf-info-example.go
@@ -2,22 +2,23 @@ package main
 
 import (
 	"fmt"
+
 	"github.com/dynamicpdf-api/go-client/endpoint"
 	"github.com/dynamicpdf-api/go-client/resource"
 )
-      
-    func main() {
-        
-      resource := resource.NewPdfResourceWithResourcePath("C:/temp/dynamicpdf-api-samples/pdf-info/fw4.pdf", "fw4.pdf")
-      text := endpoint.NewPdfInfoResource(resource)
-      
-      text.Endpoint.BaseUrl = "https://api.dynamicpdf.com/"
-      text.Endpoint.ApiKey = "DP--api-key--"
 
-      resp := text.Process()
-      res := <-resp
-	
-		  if res.IsSuccessful() == true {
-						fmt.Print(string(res.Content().Bytes()))
-		  }
-    }
+func main() {
+	basePath := "./resources/get-pdf-info-pdf-info-endpoint/"
+	resource := resource.NewPdfResourceWithResourcePath(basePath+"fw4.pdf", "fw4.pdf")
+	text := endpoint.NewPdfInfoResource(resource)
+
+	text.Endpoint.BaseUrl = "https://api.dynamicpdf.com/"
+	text.Endpoint.ApiKey = "DP--api-key--"
+
+	resp := text.Process()
+	res := <-resp
+
+	if res.IsSuccessful() == true {
+		fmt.Print(string(res.Content().Bytes()))
+	}
+}
