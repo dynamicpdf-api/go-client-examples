c6593671b1e8cf85845df137102107370866a450
James A. Brannan <jamesabrannan@hushmail.com>
2024-07-11 12:16:12 -0400

    Update image-conversion.go

--- image-conversion.go
+++ image-conversion.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/dynamicpdf-api/go-client/v2/endpoint"
 	"github.com/dynamicpdf-api/go-client/v2/input"
+	"github.com/dynamicpdf-api/go-client/v2/position"
 	"github.com/dynamicpdf-api/go-client/v2/resource"
 )
 
@@ -17,7 +18,7 @@ var outputPath string
 func init() {
 	basePath = "./resources/image-conversion/"
 	apiKey = "DP--api-key--"
-	outputPath = "./output/image-conversion-output.pdf"
+	outputPath = "./output/image-conversion-go-output.pdf"
 }
 
 func main() {
@@ -28,12 +29,25 @@ func main() {
 	imageResource1 := resource.NewImageResourceWithResourcePath(basePath+"testimage.tif", "testimage.tif")
 
 	imageInput1 := input.NewImagewithImageResource(imageResource1)
+	imageInput1.Align = position.Center
+
+	imageInput1.ExpandToFit = false
+	imageInput1.SetPageHeight(1008)
+	imageInput1.SetPageWidth(612)
+	imageInput1.VAlign = position.Top
+	imageInput1.Align = position.Center
+
 	pr.Inputs = append(pr.Inputs, imageInput1)
 
 	imageResource2 := resource.NewImageResourceWithResourcePath(basePath+"dynamicpdfLogo.png", "dynamicpdfLogo.png")
 	imageInput2 := pr.AddImage(imageResource2)
-	imageInput2.ScaleX = 25
-	imageInput2.ScaleY = 25
+	imageInput2.ExpandToFit = false
+	imageInput2.SetPageWidth(1008)
+	imageInput2.SetPageHeight(612)
+	imageInput2.VAlign = position.Middle
+	imageInput2.Align = position.Center
+	imageInput2.ScaleX = .5
+	imageInput2.ScaleY = .5
 
 	resp := pr.Process()
 	res := <-resp
