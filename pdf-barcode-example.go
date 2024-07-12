7b88c499b5e85888afbbde59d14f7968f5f2e7ce
James A. Brannan <jamesabrannan@hushmail.com>
2024-07-12 11:37:17 -0400

    minor changes

--- pdf-barcode-example.go
+++ pdf-barcode-example.go
@@ -14,7 +14,7 @@ var apiKey string
 var outputPath string
 
 func init() {
-	apiKey = "Dp--api-key--"
+	apiKey = "DP--api-key--"
 	outputPath = "./output/barcode-example-go-output.pdf"
 }
 
@@ -28,15 +28,19 @@ func main() {
 	pageInput.SetPageWidth(1008)
 
 	barcode := element.NewCode11Barcode("12345678910", element.TopCenter, 200, 50, 50)
+	//barcode.Placement = element.TopCenter
 	barcode.SetColor(color.NewRgbColorDefault().Red().Color)
 
 	pageInput.Elements = append(pageInput.Elements, barcode)
 	barcodePdf.Inputs = append(barcodePdf.Inputs, pageInput)
 
+	fmt.Print(barcodePdf.GetInstructionsJson(true))
+
 	resp := barcodePdf.Process()
 	res := <-resp
 
 	if res.IsSuccessful() == true {
+		os.Remove(outputPath)
 		os.WriteFile(outputPath,
 			res.Content().Bytes(), os.ModeType)
 	} else {
--- pdf-excel-example.go
+++ pdf-excel-example.go
@@ -35,5 +35,4 @@ func main() {
 	} else {
 		fmt.Print(res.ErrorJson())
 	}
-
 }
