c4e29a136ebd38508da8af587efc169f2d297d06
James A. Brannan <jamesabrannan@hushmail.com>
2024-07-15 12:52:02 -0400

    Update pdf-barcode-example.go

--- pdf-barcode-example.go
+++ pdf-barcode-example.go
@@ -8,18 +8,26 @@ import (
 	"github.com/dynamicpdf-api/go-client/v2/element"
 	"github.com/dynamicpdf-api/go-client/v2/endpoint"
 	"github.com/dynamicpdf-api/go-client/v2/input"
+	"github.com/dynamicpdf-api/go-client/v2/resource"
 )
 
 var apiKey string
 var outputPath string
+var basePath string
 
 func init() {
 	apiKey = "DP--api-key--"
-	outputPath = "./output/barcode-example-go-output.pdf"
+	outputPath = "./output/"
+	basePath = "./resources/merge-pdfs-pdf-endpoint/"
 }
 
 func main() {
+	barcodeNew()
+	barcodeExisting()
 
+}
+
+func barcodeNew() {
 	barcodePdf := endpoint.NewPdf()
 	barcodePdf.Endpoint.ApiKey = apiKey
 
@@ -28,7 +36,6 @@ func main() {
 	pageInput.SetPageWidth(1008)
 
 	barcode := element.NewCode11Barcode("12345678910", element.TopCenter, 200, 50, 50)
-	//barcode.Placement = element.TopCenter
 	barcode.SetColor(color.NewRgbColorDefault().Red().Color)
 
 	pageInput.Elements = append(pageInput.Elements, barcode)
@@ -40,8 +47,40 @@ func main() {
 	res := <-resp
 
 	if res.IsSuccessful() == true {
-		os.Remove(outputPath)
-		os.WriteFile(outputPath,
+		os.Remove(outputPath + "barcode-new-example-go-output.pdf")
+		os.WriteFile(outputPath + "barcode-new-example-go-output.pdf",
+			res.Content().Bytes(), os.ModeType)
+	} else {
+		fmt.Println("Error Message:" + res.ErrorMessage())
+		fmt.Print(res.ErrorJson())
+	}
+}
+
+func barcodeExisting() {
+
+	barcodePdf := endpoint.NewPdf()
+	barcodePdf.Endpoint.ApiKey = apiKey
+
+	pdfResource := resource.NewPdfResourceWithResourcePath(basePath+"DocumentA.pdf", "DocumentA.pdf")
+	pdfInput := input.NewPdfWithResource(pdfResource)
+	barcodePdf.Inputs = append(barcodePdf.Inputs, pdfInput)
+
+	pdfTemplate := element.NewTemplate()
+	pdfTemplate.Id = "Temp1"
+
+	barcode := element.NewAztecBarcodeElement("Hello World", element.TopCenter, 0, 500)
+	barcode.SetColor(color.NewRgbColorDefault().Red().Color)
+
+	pdfTemplate.Elements = append(pdfTemplate.Elements, barcode)
+
+	pdfInput.SetTemplate(pdfTemplate)
+
+	resp := barcodePdf.Process()
+	res := <-resp
+
+	if res.IsSuccessful() == true {
+		os.Remove(outputPath + "barcode-existing-example-go-output.pdf")
+		os.WriteFile(outputPath + "barcode-existing-example-go-output.pdf",
 			res.Content().Bytes(), os.ModeType)
 	} else {
 		fmt.Println("Error Message:" + res.ErrorMessage())
