c3f7672427ac3d228994d4c12f0aefd77aed20db
James A. Brannan <jamesabrannan@hushmail.com>
2025-02-11 14:57:54 -0500

    added method

--- go.mod
+++ go.mod
@@ -1,11 +1,11 @@
 module go-client-examples
 
-require github.com/dynamicpdf-api/go-client/v2 v2.2.1
+require github.com/dynamicpdf-api/go-client/v2 v2.2.2
 
 require (
-	github.com/gabriel-vasile/mimetype v1.4.5 // indirect
+	github.com/gabriel-vasile/mimetype v1.4.8 // indirect
 	github.com/google/uuid v1.6.0 // indirect
-	golang.org/x/net v0.29.0 // indirect
+	golang.org/x/net v0.35.0 // indirect
 )
 
 go 1.20
--- pdf-dlex-example.go
+++ pdf-dlex-example.go
@@ -11,16 +11,19 @@ import (
 var basePath string
 var apiKey string
 var outputPath string
+var basePathLocal string
 
 func init() {
 	basePath = "./resources/creating-pdf-pdf-endpoint/"
-	apiKey = "Dp--api-key--"
+	basePathLocal = "./resources/creating-pdf-dlex-layout/"
+	apiKey = "DP.Ugguk0HRu/CxWc+I7xeOH0NSqYZZwYdn0jYWB65rgfn2T6ImN1zUhLpS"
 	outputPath = "./output/merge-pdfs-go-output.pdf"
 }
 
 func main() {
 	processCloudPdf()
 	processLocalPdf()
+	processLocalPdfTwo()
 }
 
 func processLocalPdf() {
@@ -31,9 +34,35 @@ func processLocalPdf() {
 	theDlexResource := resource.NewDlexResourceWithPath(basePath+"SimpleReportWithCoverPage.dlex", "SimpleReportWithCoverPage.dlex")
 	pr.AddDlexWithDlexResourceNLayoutDataPath(*theDlexResource, basePath+"SimpleReportWithCoverPage.json")
 
-	additionResource := endpoint.NewDlexWithAdditionalResource(basePath+"Northwind Logo.gif", "Northwind Logo.gif")
+	pr.AddAdditionalResource(basePath+"Northwind Logo.gif", "Northwind Logo.gif")
 
 	// The dlex has an additional image in it, I'm using local dlex, need to add to the pdf endpoint
+
+	resp := pr.Process()
+	res := <-resp
+
+	if res.IsSuccessful() == false {
+		if res.ClientError() != nil {
+			fmt.Print("Failed with error: " + res.ClientError().Error())
+		} else {
+			fmt.Print("Failed with error: " + res.ErrorJson())
+		}
+	} else {
+		os.WriteFile(basePath+"pdf-dlex-pdf-local-two-output.pdf",
+			res.Content().Bytes(), os.ModeType)
+	}
+}
+
+func processLocalPdfTwo() {
+
+	pr := endpoint.NewPdf()
+	pr.Endpoint.ApiKey = apiKey
+
+	theDlexResource := resource.NewDlexResourceWithPath(basePathLocal+"ExampleTemplate.dlex", "ExampleTemplate.dlex")
+	pr.AddDlexWithDlexResourceNLayoutDataPath(*theDlexResource, basePathLocal+"ExampleTemplate.json")
+
+	pr.AddAdditionalResource(basePathLocal+"signature-one.png", "signature-one.png")
+	pr.AddAdditionalResource(basePathLocal+"template_example.pdf", "template_example.pdf")
 
 	resp := pr.Process()
 	res := <-resp
@@ -50,10 +79,9 @@ func processLocalPdf() {
 	}
 }
 
-func processCloudPdf(baseUrl string, apiKey string, basePath string) {
+func processCloudPdf() {
 
 	pr := endpoint.NewPdf()
-	pr.Endpoint.BaseUrl = baseUrl
 	pr.Endpoint.ApiKey = apiKey
 
 	layoutDataResource := resource.NewLayoutDataResource(basePath+"SimpleReportWithCoverPage.json", "SimpleReportWithCoverPage.json")
